377ffbdddf9fe0cc4de7bfa787c6191e4a9b6784
Yauheni Matach <zhenya.matach@gmail.com>
2026-01-26 14:03:11 +0300

    add httpStatutes.ts, and app structure configuration

---
+++ src/core/types/httpStatutes.ts
@@ -0,0 +1,11 @@
+export enum HttpStatus {
+    Ok_200 = 200,
+    Created_201 = 201,
+    NoContent_204 = 204,
+
+    BadRequest_400 = 400,
+    Unauthorized_401 = 401,
+    NotFound_404 = 404,
+
+    InternalServerError_500 = 500
+}
