bf7713e52965e571e886a935594eacd4b0fde54b
Yauheni Matach <zhenya.matach@gmail.com>
2026-01-26 17:47:26 +0300

    add blog-input.dto.ts and post-input.dto.ts

---
+++ src/blogs/dto/blog-input.dto.ts
@@ -0,0 +1,5 @@
+export type blogInputDto = {
+    name: string;
+    description: string;
+    websiteUrl: string;
+}
---
+++ src/posts/dto/post-input.dto.ts
@@ -0,0 +1,6 @@
+export type postInputDto = {
+    title: string;
+    shortDescription: string;
+    content: string;
+    blogId: string
+}
