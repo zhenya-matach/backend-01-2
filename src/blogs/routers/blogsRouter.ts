85cc85ca6613dfd7ab59685cb2c21a01a47e1092
Yauheni Matach <zhenya.matach@gmail.com>
2026-02-04 00:21:06 +0300

    fix deleteBlogHandler

--- src/blogs/routers/blogsRouter.ts
+++ src/blogs/routers/blogsRouter.ts
@@ -6,6 +6,7 @@ import {getBlogListHandler} from "./handlers/get-blog-list.handler";
 import {getBlogHandler} from './handlers/get-blog.handler';
 import {createBlogHandler} from './handlers/create-blog.handler';
 import {updateBlogHandler} from './handlers/update-blog.handler';
+import {deleteBlogHandler} from './handlers/delete-blog.handler';
 
 
 export const blogsRouter = Router();
@@ -19,4 +20,4 @@ blogsRouter
 
     .put('/:id', idValidation, blogInputDtoValidation, inputValidationResultMiddleware, updateBlogHandler)
 
-    .delete('/:id', idValidation, inputValidationResultMiddleware)
+    .delete('/:id', idValidation, inputValidationResultMiddleware, deleteBlogHandler);
