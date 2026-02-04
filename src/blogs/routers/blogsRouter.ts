6042337968ec7c3b0743b8fb6ddba053275c532e
Yauheni Matach <zhenya.matach@gmail.com>
2026-02-04 14:03:54 +0300

    Added base64 authorization to post, put, and delete blog endpoints

--- src/blogs/routers/blogsRouter.ts
+++ src/blogs/routers/blogsRouter.ts
@@ -7,6 +7,7 @@ import {getBlogHandler} from './handlers/get-blog.handler';
 import {createBlogHandler} from './handlers/create-blog.handler';
 import {updateBlogHandler} from './handlers/update-blog.handler';
 import {deleteBlogHandler} from './handlers/delete-blog.handler';
+import {superAdminGuardMiddleware} from '../../auth/middlewares/super-admin.guard-middleware';
 
 
 export const blogsRouter = Router();
@@ -16,8 +17,8 @@ blogsRouter
 
     .get('/:id', idValidation, inputValidationResultMiddleware, getBlogHandler)
 
-    .post('', blogInputDtoValidation, inputValidationResultMiddleware, createBlogHandler)
+    .post('', superAdminGuardMiddleware, blogInputDtoValidation, inputValidationResultMiddleware, createBlogHandler)
 
-    .put('/:id', idValidation, blogInputDtoValidation, inputValidationResultMiddleware, updateBlogHandler)
+    .put('/:id', superAdminGuardMiddleware, idValidation, blogInputDtoValidation, inputValidationResultMiddleware, updateBlogHandler)
 
-    .delete('/:id', idValidation, inputValidationResultMiddleware, deleteBlogHandler);
+    .delete('/:id', superAdminGuardMiddleware, idValidation, inputValidationResultMiddleware, deleteBlogHandler);
