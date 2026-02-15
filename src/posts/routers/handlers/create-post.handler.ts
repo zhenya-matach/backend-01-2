6cbc7754b23e9b3fc1e21f0c5197c7a4e3e0638b
Yauheni Matach <zhenya.matach@gmail.com>
2026-02-16 02:35:13 +0300

    create-post.handler has been rewritten for MongoDB, added mapper for postOutputModel

--- src/posts/routers/handlers/create-post.handler.ts
+++ src/posts/routers/handlers/create-post.handler.ts
@@ -1,40 +1,43 @@
 import {Request, Response} from 'express';
-import {db} from '../../../db/db';
 import {createErrorMessages} from '../../../core/utils/error.utils';
 import {postsRepository} from '../../repositories/posts.repository';
 import {blogsRepository} from '../../../blogs/repositories/blogs.repository';
 import {HttpStatus} from '../../../core/types/httpStatutes';
-import {Post} from '../../types/post';
-import {postInputDto} from '../../dto/post-input.dto';
+import {PostMongoModel} from '../../types/postMongoModel';
+import {PostInputModel} from '../../types/postInputModel';
 import {ValidationErrorDto} from '../../../core/types/validationError.dto';
+import {PostOutputModel} from '../../types/postOutputModel';
+import {mapToPostOutputModel} from '../mappers/mapToPostOutputModel';
 
-export function createPostHandler(req: Request<{},{},postInputDto>,
-                                  res: Response<ValidationErrorDto | Post>) {
+export async function createPostHandler(req: Request<{}, {}, PostInputModel>,
+                                        res: Response<ValidationErrorDto | PostOutputModel>) {
+    try {
+        const blogId = req.body.blogId;
+        const foundBlog = await blogsRepository.findById(blogId);
 
-    const blogId = req.body.blogId;
-    const blog = blogsRepository.findById(blogId);
+        if (!foundBlog) {
+            return res
+                .status(HttpStatus.BadRequest_400)
+                .send(
+                    createErrorMessages([{field: 'blogId', message: 'Blog not exist'}]),
+                );
+        }
 
-    if (!blog) {
-        return res
-            .status(HttpStatus.BadRequest_400)
-            .send(
-                createErrorMessages([{field: 'blogId', message: 'Blog not exist'}]),
-            );
+        const newPost: PostMongoModel = {
+            title: req.body.title,
+            shortDescription: req.body.shortDescription,
+            content: req.body.content,
+            blogId: req.body.blogId,
+            blogName: foundBlog.name,
+            createdAt: new Date().toISOString()
+        };
+
+        const createdPost = await postsRepository.create(newPost);
+        const postOutputModel = mapToPostOutputModel(createdPost);
+        res
+            .status(HttpStatus.Created_201)
+            .send(postOutputModel);
+    } catch (e: unknown) {
+        res.status(HttpStatus.InternalServerError_500);
     }
-
-    const newPost: Post = {
-        id: db.posts.length ? (db.posts[db.posts.length - 1].id + 1).toString() : '1',
-        title: req.body.title,
-        shortDescription: req.body.shortDescription,
-        content: req.body.content,
-        blogId: req.body.blogId,
-        blogName: blog.name,
-        createdAt: new Date().toISOString()
-    };
-
-    postsRepository.create(newPost);
-
-    res
-        .status(HttpStatus.Created_201)
-        .send(newPost);
 }
