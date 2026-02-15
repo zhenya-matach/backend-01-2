9c370fee662d14d422bd9ab3c22fc02b2aa9b3b6
Yauheni Matach <zhenya.matach@gmail.com>
2026-02-16 01:16:40 +0300

    fix blogModels in mongo db

--- src/db/mongo.db.ts
+++ src/db/mongo.db.ts
@@ -1,14 +1,14 @@
 import {Collection, Db, MongoClient} from 'mongodb';
-import {Blog} from '../blogs/types/blog';
-import {Post} from '../posts/types/post';
+import {BlogMongoModel} from '../blogs/types/blogMongoModel';
+import {PostMongoModel} from '../posts/types/postMongoModel';
 import {SETTINGS} from '../core/settings/settings';
 
 const BLOG_COLLECTION_NAME = 'blogs';
 const POST_COLLECTION_NAME = 'posts';
 
 export let client: MongoClient;
-export let blogCollection: Collection<Blog>;
-export let postCollection: Collection<Post>;
+export let blogCollection: Collection<BlogMongoModel>;
+export let postCollection: Collection<PostMongoModel>;
 
 // Подключения к бд
 export async function runDB(url: string): Promise<void> {
@@ -16,8 +16,8 @@ export async function runDB(url: string): Promise<void> {
     const db: Db = client.db(SETTINGS.DB_NAME);
 
     //Инициализация коллекций
-    blogCollection = db.collection<Blog>(BLOG_COLLECTION_NAME);
-    postCollection = db.collection<Post>(POST_COLLECTION_NAME);
+    blogCollection = db.collection<BlogMongoModel>(BLOG_COLLECTION_NAME);
+    postCollection = db.collection<PostMongoModel>(POST_COLLECTION_NAME);
 
     try {
         await client.connect();
