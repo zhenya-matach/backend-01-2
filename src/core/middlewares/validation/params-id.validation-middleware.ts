507c92c42c829f7db4b066421b4dec0beaaa8e8f
Yauheni Matach <zhenya.matach@gmail.com>
2026-02-16 01:24:43 +0300

    added id validation that checks objectID

--- src/core/middlewares/validation/params-id.validation-middleware.ts
+++ src/core/middlewares/validation/params-id.validation-middleware.ts
@@ -4,4 +4,4 @@ export const idValidation = param('id')
     .exists().withMessage('ID is required')
     .isString().withMessage('ID must be a string')
     .isLength({min: 1}).withMessage('ID must not be empty')
-    .isNumeric().withMessage('ID must be a numeric string');
+    .isMongoId().withMessage('Incorrect format of ObjectId');
