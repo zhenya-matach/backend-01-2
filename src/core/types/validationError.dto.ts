69adde172bfea2e0033ca61ca41cb0050563fb9c
Yauheni Matach <zhenya.matach@gmail.com>
2026-02-16 03:10:23 +0300

    fixed a bug in the name of the error collector "errorsMessage"

--- src/core/types/validationError.dto.ts
+++ src/core/types/validationError.dto.ts
@@ -1,3 +1,3 @@
 import {ValidationErrorType} from "./validationErrorType";
 
-export type ValidationErrorDto = { errorMessages: ValidationErrorType[] };
+export type ValidationErrorDto = { errorsMessages: ValidationErrorType[] };
